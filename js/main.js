de1e000df848c4723a6a6d7830a388de30f3e0a1
AlexKolbe <helping5@yandex.ru>
2025-11-07 15:45:43 +0300

    сверстали модалку и она открывается и закрывается

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const modalWindow = document.querySelector(".modal");
+const openModals = document.querySelectorAll(".modal__open");
+const closeModal = document.querySelector(".modal__close");
+const body = document.querySelector("body");
+const modalButton = document.querySelector(".modal__button");
+
+openModals.forEach((item) => {
+  item.addEventListener("click", () => {
+    modalWindow.style.display = "flex";
+    body.classList.add("noscroll");
+  });
+});
+
+closeModal.addEventListener("click", () => {
+  modalWindow.style.display = "none";
+  body.classList.remove("noscroll");
+});
+
+// Закрытие при клике вне модального окна
+modalWindow.addEventListener("click", (e) => {
+  if (e.target === modalWindow) {
+    modalWindow.style.display = "none";
+    body.classList.remove("noscroll");
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && modalWindow.style.display === "flex") {
+    modalWindow.style.display = "none";
+    body.classList.remove("noscroll");
+  }
+});
+
+modalButton.addEventListener("click", () => {
+  modalWindow.style.display = "none";
+  body.classList.remove("noscroll");
+});
