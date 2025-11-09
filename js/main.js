0726d853d53a01e39a44661ce55b70acaf9f6940
Alexandr Kolesnikov <89801214+AlexKolbe@users.noreply.github.com>
2025-11-09 21:32:21 +0300

    новый интенсив

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const openModals = document.querySelectorAll(".modal__open");
 const closeModal = document.querySelector(".modal__close");
 const body = document.querySelector("body");
 const modalButton = document.querySelector(".modal__button");
+const contens = document.querySelectorAll('.program-line__content')
+
+contens.forEach((elem) => {
+  const title = elem.querySelector('.program-line__title');
+  const descr = elem.querySelector('.program-line__descr');
+
+  title.addEventListener('click', () => {
+    descr.classList.toggle('active')
+  })
+})
 
 openModals.forEach((item) => {
   item.addEventListener("click", () => {
