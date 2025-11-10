d7b6c5c4c5ed363893040d1848f1d4733e4a13e8
AlexKolbe <helping5@yandex.ru>
2025-11-10 11:53:46 +0300

    two lesson another

---
+++ js/accordion.js
@@ -0,0 +1,11 @@
+// работа аккордиона
+const contens = document.querySelectorAll(".program-line__content");
+
+contens.forEach((elem) => {
+  const title = elem.querySelector(".program-line__title");
+  const descr = elem.querySelector(".program-line__descr");
+
+  title.addEventListener("click", () => {
+    descr.classList.toggle("active");
+  });
+});
