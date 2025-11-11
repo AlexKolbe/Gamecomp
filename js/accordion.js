66993e7b17f6ea7d5bdcb58dd5c6a7f974fcb1b8
AlexKolbe <helping5@yandex.ru>
2025-11-11 11:31:26 +0300

    added fetch to server

--- js/accordion.js
+++ js/accordion.js
@@ -1,11 +1,16 @@
 // работа аккордиона
-const contens = document.querySelectorAll(".program-line__content");
 
-contens.forEach((elem) => {
-  const title = elem.querySelector(".program-line__title");
-  const descr = elem.querySelector(".program-line__descr");
+const accordeon = () => {
+  const contens = document.querySelectorAll(".program-line__content");
 
-  title.addEventListener("click", () => {
-    descr.classList.toggle("active");
+  contens.forEach((elem) => {
+    const title = elem.querySelector(".program-line__title");
+    const descr = elem.querySelector(".program-line__descr");
+
+    title.addEventListener("click", () => {
+      descr.classList.toggle("active");
+    });
   });
-});
+};
+
+accordeon();
